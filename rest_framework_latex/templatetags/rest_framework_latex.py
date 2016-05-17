672535e273e218858dafcf7e3c8d316c701598bd
Scott Walton <scott.walton@mypebble.co.uk>
2016-05-17 15:36:35 +0100

    Removed the template tag we don't need #9

--- rest_framework_latex/templatetags/rest_framework_latex.py
+++ rest_framework_latex/templatetags/rest_framework_latex.py
@@ -1,6 +1,4 @@
 from django import template
-from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
 from django.utils.safestring import mark_safe
 
 register = template.Library()
@@ -20,16 +18,3 @@ def latex_safe(value):
     value = value.replace('~', '\\textasciitidle{}')
     value = value.replace('\\textbackslash\\{\\}', '\\textbackslash{}')
     return mark_safe(value)
-
-
-@register.simple_tag
-def latex_resources():
-    """Return the LATEX_RESOURCES setting with a trailing /
-    """
-    resources = getattr(settings, 'LATEX_RESOURCES', None)
-    if resources is None:
-        raise ImproperlyConfigured(
-            'LATEX_RESOURCES is not defined in settings')
-    if not resources.endswith('/'):
-        resources = u'{}/'.format(resources)
-    return resources
--- tests/tests/testtemplatetags/tests.py
+++ tests/tests/testtemplatetags/tests.py
@@ -1,38 +1,11 @@
 from unittest import TestCase
 
-from mock import patch
-
-from django.core.exceptions import ImproperlyConfigured
 from rest_framework_latex.templatetags import rest_framework_latex as tags
 
 
 class TemplateTagTestCase(TestCase):
     """Test the LaTeX-specific template tags
     """
-
-    @patch('rest_framework_latex.templatetags.rest_framework_latex.settings')
-    def test_latex_resources(self, settings):
-        """Test the latex_resources templatetag
-        """
-        settings.LATEX_RESOURCES = '/home/test_value/'
-        self.assertEqual(tags.latex_resources(), '/home/test_value/')
-
-    @patch('rest_framework_latex.templatetags.rest_framework_latex.settings')
-    def test_latex_resources_trailing_slash(self, settings):
-        """The template tag appends a trailing /
-        """
-        settings.LATEX_RESOURCES = '/home/test_value'
-        self.assertEqual(tags.latex_resources(), '/home/test_value/')
-
-    def test_latex_improperly_configured(self):
-        """If LATEX_RESOURCES isn't set, raise an exception
-        """
-        try:
-            tags.latex_resources()
-        except ImproperlyConfigured as e:
-            self.assertEqual(
-                e.message, 'LATEX_RESOURCES is not defined in settings')
-
     def test_escape_tags(self):
         """Escape the latex tags in a user-entered string.
         """
