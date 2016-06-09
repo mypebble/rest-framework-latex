358a7b6fddbf3d1cf5301a8cec746fdbf6041313
Joe Simpson <headbangerkenny@gmail.com>
2016-06-09 11:34:45 +0100

    Tests + Fix circumflex

--- tests/tests/testtemplatetags/tests.py
+++ tests/tests/testtemplatetags/tests.py
@@ -11,25 +11,27 @@ class TemplateTagTestCase(TestCase):
     def test_escape_tags(self):
         """Escape the latex tags in a user-entered string.
         """
-        simple_replace = ('#', '$', '%', '^', '_', '{', '}')
+        simple_replace = ('#', '$', '%', '_', '{', '}', '&')
         for character in simple_replace:
             escaped_char = u'\\{}'.format(character)
             self.assertEqual(tags.latex_safe(character), escaped_char)
 
         self.assertEqual(tags.latex_safe('\\'), '\\textbackslash{}')
         self.assertEqual(tags.latex_safe('~'), '\\textasciitidle{}')
+        self.assertEqual(tags.latex_safe('^'), '\\^{}')
 
     def test_escape_collected(self):
         """Collect the tags and escape in one go.
         """
-        simple_replace = ('#', '$', '%', '^', '_', '{', '}')
+        simple_replace = ('#', '$', '%', '_', '{', '}', '&')
         escaped_backslash = '\\textbackslash{}'
         escaped_tilde = '\\textasciitidle{}'
+        escaped_circumflex = '\\^{}'
 
-        entry_string = ''.join(simple_replace + ('\\', '~'))
+        entry_string = ''.join(simple_replace + ('\\', '~', '^'))
         escaped_string = ''.join(
             tuple('\\{}'.format(c) for c in simple_replace) +
-            (escaped_backslash, escaped_tilde))
+            (escaped_backslash, escaped_tilde, escaped_circumflex))
 
         self.assertEqual(tags.latex_safe(entry_string), escaped_string)
 
