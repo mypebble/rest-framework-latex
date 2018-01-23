d32bf695f963db386e79c3b23d85063d140ff04c
Scott Walton <scott.walton@mypebble.co.uk>
2018-01-23 11:28:30 +0000

    Import correct TestCase

--- tests/tests/testrenderers/tests/test_latex.py
+++ tests/tests/testrenderers/tests/test_latex.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 try:
     from unittest import mock, TestCase
 except ImportError:
+    from unittest import TestCase
     import mock
 import six
 
